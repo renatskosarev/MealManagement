a75435ecaf925f590a2ace3b46d2195f4b0fd720
Renat Skosarev <skosarrrev@gmail.com>
2023-02-23 04:04:00 +0500

    Update DB init & populate files

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,25 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
 
-INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+INSERT INTO user_roles (role, user_id)
+VALUES ('ROLE_USER', 100000),
+       ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2023-02-21 9:00', 'Завтрак', 560, 100000),
+       ('2023-02-21 14:40', 'Обед', 490, 100000),
+       ('2023-02-21 18:10', 'Быстрый перекус', 120, 100000),
+       ('2023-02-21 21:00', 'Ужин', 900, 100000),
+       ('2023-02-22 10:00', 'Завтрак', 400, 100000),
+       ('2023-02-21 15:50', 'Обед', 600, 100000),
+       ('2023-02-21 22:00', 'Ужин', 1100, 100000),
+       ('2023-02-22 10:00', '(админ) Завтрак', 400, 100001),
+       ('2023-02-21 15:50', '(админ) Обед', 600, 100001),
+       ('2023-02-21 22:00', '(админ) Ужин', 1100, 100001);
