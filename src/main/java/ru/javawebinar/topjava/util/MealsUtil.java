032c74b7973fd173c0cb5cd13e1ee68bf6422303
Renat Skosarev <95104142+skosarevv@users.noreply.github.com>
2023-04-16 16:30:56 +0500

    9.04: Binding

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -16,7 +16,6 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class MealsUtil {
-    public static final int DEFAULT_CALORIES_PER_DAY = 2000;
 
     public static final List<Meal> MEALS = Arrays.asList(
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
